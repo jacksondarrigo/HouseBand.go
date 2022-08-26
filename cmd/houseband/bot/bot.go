f77e67d177f0ca06ba34140858a40b3fbb332e57
Jackson D'Arrigo <jackson@darrigo.dev>
2022-08-26 01:20:25 -0400

    Removing web server functions - moving web server development to separate branch

--- cmd/houseband/bot/bot.go
+++ cmd/houseband/bot/bot.go
@@ -1,7 +1,6 @@
 package bot
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
@@ -44,66 +43,8 @@ func (bot *Bot) Run(resetCommands bool) {
 	}
 	defer bot.Close()
 
-	bot.startWebServer()
-	defer bot.stopWebServer()
-
 	fmt.Println("HouseBandTest is now running.  Press CTRL-C to exit.")
 	signals := make(chan os.Signal, 1)
 	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-signals
-
-	// Remove all existing commands on exit - prevents users from issuing a command when the bot is unavailable, also make sures old commands are deleted when removed from the codebase
-	//bot.deleteCommands()
-}
-
-func (bot *Bot) stopWebServer() {
-	err := bot.webServer.Close()
-	if err != nil {
-		fmt.Println("Error closing web server: ", err)
-	}
-}
-
-func (bot *Bot) startWebServer() {
-	mux := http.NewServeMux()
-	webHandler := func(w http.ResponseWriter, r *http.Request) {
-		body, err := json.Marshal(bot.Session.State)
-		if err != nil {
-			body = []byte("Error retrieving bot state: " + err.Error())
-		}
-		fmt.Fprintf(w, string(body))
-		// var guilds []string = []string{}
-		// for _, guild := range bot.State.Guilds {
-		// 	guilds = append(guilds, guild.Name)
-		// }
-		// body, err := json.Marshal(guilds)
-		// if err != nil {
-		// 	body = []byte(strings.Join(guilds, "\n "))
-		// }
-		// fmt.Fprintf(w, "Guilds:\n"+string(body)+"\n")
-		// var voiceConnections []string = []string{}
-		// for _, vc := range bot.VoiceConnections {
-		// 	guild, err := bot.Guild(vc.GuildID)
-		// 	if err != nil {
-		// 		continue
-		// 	}
-		// 	channel, err := bot.Channel(vc.ChannelID)
-		// 	if err != nil {
-		// 		continue
-		// 	}
-		// 	voiceConnections = append(voiceConnections, guild.Name+": "+channel.Name)
-		// }
-		// body, err = json.Marshal(voiceConnections)
-		// if err != nil {
-		// 	body = []byte(strings.Join(voiceConnections, "\n "))
-		// }
-		// fmt.Fprintf(w, "\nVoice Connections:\n"+string(body)+"\n")
-	}
-	mux.HandleFunc("/", webHandler)
-	bot.webServer = &http.Server{Addr: ":8080", Handler: mux}
-	go func() {
-		err := bot.webServer.ListenAndServe()
-		if err != nil {
-			fmt.Println("Error creating web server: ", err)
-		}
-	}()
 }
